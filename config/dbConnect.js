fe7a0c7d73727b63bd2632ce030a752d7f153e33
Bilal Shahid <bcsm-f21-249@superior.edu.pk>
2024-06-19 08:47:19 -0700

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const url = process.env.MONGODB_URL
 const dbConnect = async () => {
   mongoose
-    .connect(url)
+    .connect("mongodb+srv://bcsmf21249:pvcvh38rXay3Wxl7@mernclustor.qf3zmjk.mongodb.net/MERNDB?retryWrites=true&w=majority&appName=MernClustor")
     .then(() => {
       console.log("mongodb connected");
     })
